239099eb4bac0395608ff75231c10ba59f926153
A_Ostrowski <andypandy1010@hotmail.com>
2025-06-10 12:46:57 -0500

    added the code for captioning, and added caption pics to the home, index, and images pages

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const initModels = require('./models/init-models');
+const bcrypt = require('bcrypt');
+const jwt = require("jsonwebtoken");
 const { sequelize } = require('./models');
 
 const app = express();
@@ -7,6 +9,7 @@ const models = initModels(sequelize);
 
 
 const PORT = process.env.PORT || 4001;
+const SECRET = "coffeeCakes"
 
 app.use(express.static('public'));
 app.use(express.json());
@@ -15,7 +18,26 @@ app.use(express.json());
 app.get("/", async (req, res) =>{
     try {
         const pics = await models.pics.findAll();
-        return res.json(pics);
+        const captionPics = await models.captions.findAll();
+
+        const captionArray = [];
+        for  (var i=0; i < captionPics.length; i++){
+            console.log(captionPics[i]);
+            const picsInfo = await models.pics.findOne({where: captionPics[i].pics_id});
+            const userInfo = await models.users.findOne({where: captionPics[i].user_id});
+            const captionInfo = {
+                pics_id: picsInfo.id,
+                pics_name: picsInfo.name,
+                username: userInfo.username,
+                caption: captionPics[i].caption,
+                createdAt: captionPics[i].createdAt,
+                pics_data: picsInfo.image,
+            }
+            captionArray.push(captionInfo);
+        }
+
+        const jsonArray = [pics, captionArray]
+        return res.json(jsonArray);
     } catch (error) {
         console.log(error);
         return res.status(500).json({err: "something broke"});
@@ -25,7 +47,26 @@ app.get("/", async (req, res) =>{
 app.get("/index", async (req, res) =>{
     try {
         const pics = await models.pics.findAll();
-        return res.json(pics);
+        const captionPics = await models.captions.findAll();
+
+        const captionArray = [];
+        for  (var i=0; i < captionPics.length; i++){
+            console.log(captionPics[i]);
+            const picsInfo = await models.pics.findOne({where: captionPics[i].pics_id});
+            const userInfo = await models.users.findOne({where: captionPics[i].user_id});
+            const captionInfo = {
+                pics_id: picsInfo.id,
+                pics_name: picsInfo.name,
+                username: userInfo.username,
+                caption: captionPics[i].caption,
+                createdAt: captionPics[i].createdAt,
+                pics_data: picsInfo.image,
+            }
+            captionArray.push(captionInfo);
+        }
+
+        const jsonArray = [pics, captionArray]
+        return res.json(jsonArray);
     } catch (error) {
         console.log(error);
         return res.status(500).json({err: "something broke"});
@@ -35,12 +76,32 @@ app.get("/index", async (req, res) =>{
 app.get("/images", async (req, res) =>{
     try {
         const pics = await models.pics.findAll();
-        return res.json(pics);
+        const captionPics = await models.captions.findAll();
+
+        const captionArray = [];
+        for  (var i=0; i < captionPics.length; i++){
+            console.log(captionPics[i]);
+            const picsInfo = await models.pics.findOne({where: captionPics[i].pics_id});
+            const userInfo = await models.users.findOne({where: captionPics[i].user_id});
+            const captionInfo = {
+                pics_id: picsInfo.id,
+                pics_name: picsInfo.name,
+                username: userInfo.username,
+                caption: captionPics[i].caption,
+                createdAt: captionPics[i].createdAt,
+                pics_data: picsInfo.image,
+            }
+            captionArray.push(captionInfo);
+        }
+
+        const jsonArray = [pics, captionArray]
+        return res.json(jsonArray);
     } catch (error) {
         console.log(error);
         return res.status(500).json({err: "something broke"});
     }
 });
+
 
 
 app.get("/image/:name", async (req, res) =>{
@@ -56,36 +117,116 @@ app.get("/image/:name", async (req, res) =>{
     }
 });
 
-app.post("/image/:name", async (req, res) =>{
-    const {body} = req.body;
+app.post("/image/:name",
+    async (req, res,next) =>{
     try {
-        
+        const token = req.headers['authorization'].split(" ")[1];
+        const decoded = jwt.verify(token, SECRET);
+        console.log(decoded);
+        if(decoded){
+            const userCheck = await models.users.findOne({
+                where: {username: decoded.username},
+            });
+            if(userCheck){
+                const passwordValid = await bcrypt.compare(decoded.password, userCheck.password);
+                if(passwordValid){
+                    next();
+                }else{
+                    return res.status(400).json({ err : "Password Incorrect" });
+                }
+            }else{
+                return res.status(404).json({ err : "User does not exist" });
+            }
+        }
     } catch (error) {
-        
+       return res.status(401).json({"msg":"Could not Authenticate"});
     }
+},
+async (req, res, next)=>{
+        try {
+            const userCheck = await models.users.findOne({
+                where: {username: req.body.username},
+            });
+            const picsID = await models.pics.findOne({
+                where: {name: req.params.name},
+            });
+            if(!userCheck){
+                return res.status(404).json({'msg':"User not found"});
+            }if (!picsID) {
+               return res.status(404).json({'msg':"Image not found"}); 
+            }else{
+                var newCaption = {
+                    pics_id : picsID.id,
+                    user_id : userCheck.id,
+                    caption : req.body.caption,
+                }
+
+                createCaption = await models.captions.create(newCaption);
+                return res.status(201).json(newCaption);
+            }            
+        } catch (error) {
+            return res.status(500).json({err: "something broke "+ error});
+        }
+
 });
 
 
 app.get("/registration", async (req, res) =>{
-    
+     res.json({username: "___", password: "___"});
 });
 
 app.post("/registration", async (req, res) =>{
+    try {
+        const userCheck = await models.users.findOne({
+            where: {username: req.body.username},
+        });
+
+        if (userCheck) {
+            return res.json({message: "Username taken"});
+        } else {
+            const salt = await bcrypt.genSalt(9);
+            const hashPassword = await bcrypt.hash(req.body.password, salt);
+     
+            var newUser = {
+                username: req.body.username,
+                password: hashPassword
+            };
     
+            createUser = await models.users.create(newUser);
+            
+            return res.status(201).json(createUser);  
+        }
+    } catch (error) {
+        return res.status(500).json({err: "something broke"});
+    }
 });
 
 
 app.get("/login", async (req, res) =>{
-    
+    res.json({username: "___", password: "___"});
 });
 
 app.post("/login", async (req, res) =>{
-    
-});
+    try {
+        const userCheck = await models.users.findOne({
+            where: {username: req.body.username},
+        });
 
-
-app.get("/logout", async (req, res) =>{
-    
+        if(userCheck){
+            const passwordValid = await bcrypt.compare(req.body.password, userCheck.password);
+            if(passwordValid){
+                token = jwt.sign({id: userCheck.id, username: userCheck.username}, SECRET);
+               return res.status(200).json({token: token});
+            }
+            else{
+               return res.status(400).json({ err : "Password Incorrect" });
+            }
+        }else{
+            return res.status(404).json({ err : "User does not exist" });
+        }
+    } catch (error) {
+        return res.status(500).json({err: "something broke"});
+    }
 });
 
 app.use((req, res, next) => {
